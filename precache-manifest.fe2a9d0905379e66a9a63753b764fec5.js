76f27fc5bc9fa9b230a3341f3c2371bfaa880c05
jamisonwight <jmsnwight@gmail.com>
2021-04-22 11:06:57 -0500

    deploy

---
+++ precache-manifest.fe2a9d0905379e66a9a63753b764fec5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17395937c74859a4a381",
+    "url": "/wine-talks/css/app.485d271a.css"
+  },
+  {
+    "revision": "944458eb798d7ac569c741695b2a04dc",
+    "url": "/wine-talks/index.html"
+  },
+  {
+    "revision": "17395937c74859a4a381",
+    "url": "/wine-talks/js/app.fda3c64b.js"
+  },
+  {
+    "revision": "e342346e9facc6db038b",
+    "url": "/wine-talks/js/chunk-vendors.2b0ebfa0.js"
+  },
+  {
+    "revision": "80572b450316a42be5a7babd3b00a3dd",
+    "url": "/wine-talks/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/wine-talks/robots.txt"
+  }
+]);
